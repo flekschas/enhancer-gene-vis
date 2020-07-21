275cd00d209b767dc899f9970f144ee7b34ffc89
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-07-21 11:22:50 -0400

    Updates

---
+++ precache-manifest.88a0da7876b061e94a11cc94b26adab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5db687a055fdf6acc85178bd2b65647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "444c784794c691d025b9",
+    "url": "/static/css/2.99e59d71.chunk.css"
+  },
+  {
+    "revision": "e76f7a4ab1e5cafab85a",
+    "url": "/static/css/main.2bcd5e50.chunk.css"
+  },
+  {
+    "revision": "444c784794c691d025b9",
+    "url": "/static/js/2.0ddab056.chunk.js"
+  },
+  {
+    "revision": "440cf14f106c2ca544689cd63b531980",
+    "url": "/static/js/2.0ddab056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76f7a4ab1e5cafab85a",
+    "url": "/static/js/main.fabea71a.chunk.js"
+  },
+  {
+    "revision": "4958f385790b0b28a93e",
+    "url": "/static/js/runtime-main.84bb58a6.js"
+  }
+]);
