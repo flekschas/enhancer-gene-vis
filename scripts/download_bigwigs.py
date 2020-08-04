1ad1bcbd6966904c08e8f3e96e763498af6b14f3
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-08-04 13:20:37 -0400

    Download Jesse's DNA accessibility files

---
+++ scripts/download_bigwigs.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import pathlib
+import shutil
+import urllib.request as request
+from contextlib import closing
+
+def get_file_urls():
+    from ftplib import FTP
+
+    protocol = 'ftp://'
+    host = 'ftp.broadinstitute.org'
+    base_path = '/outgoing/lincRNA/ABC/bigWig/'
+
+    with FTP('ftp.broadinstitute.org') as ftp:
+        ftp.login()
+        file_urls = []
+        for file_name, facts in ftp.mlsd('outgoing/lincRNA/ABC/bigWig/'):
+            if facts['type'] != 'dir' and file_name.endswith('accessibility.bw'):
+                file_urls.append(
+                    protocol + host + base_path + file_name
+                )
+
+    return file_urls
+
+def download_file_from_ftp(
+    url: str,
+    base: str = ".",
+    dir: str = "data",
+    overwrite: bool = False,
+):
+    _, filename = os.path.split(url)
+    filepath = os.path.join(base, dir, filename)
+
+    if pathlib.Path(filepath).is_file() and not overwrite:
+        print(f'{filename} already exist! To overwrite set `overwrite=True`')
+        return
+
+    print(f'Download {filename} ', end='', flush=True)
+
+    with closing(request.urlopen(url)) as r:
+        with open(filepath, 'wb') as f:
+            shutil.copyfileobj(r, f)
+
+    print('done!', flush=True)
+
+
+def download(base: str = ".", dir: str = "data", overwrite: bool = False):
+    pathlib.Path(dir).mkdir(parents=True, exist_ok=True)
+
+    file_urls = get_file_urls()
+
+    k = 0
+    for file_url in file_urls:
+        download_file_from_ftp(file_url, base=base, dir=dir, overwrite=overwrite)
+        k += 1
+        if k > 2:
+            break
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Download bigwigs.')
+    parser.add_argument('-b', '--base', type=str, default='.', help='Base directory')
+    parser.add_argument('-d', '--dir', type=str, default='data', help='Data directory (relative to the base directory)')
+    parser.add_argument('-o', '--overwrite', action="store_true", help="if true overwrite files")
+    args = parser.parse_args()
+
+    download(args.base, args.dir, args.overwrite)
