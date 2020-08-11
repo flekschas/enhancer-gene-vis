533eadd332ab6fff296cd920e473027837a84349
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-08-11 12:58:48 -0400

    Implement annotation overlay track

---
+++ src/annotation-overlay-meta-track.js
@@ -0,0 +1,125 @@
+function createAnnotationOverlayMetaTrack(HGC, ...args) {
+  if (!new.target) {
+    throw new Error(
+      'Uncaught TypeError: Class constructor cannot be invoked without "new"'
+    );
+  }
+
+  // Services
+  const { pubSub } = HGC.services;
+
+  class AnnotationOverlayMetaTrack {
+    constructor(context, options) {
+      const { definition, getTrackObject, onNewTilesLoaded: animate } = context;
+
+      this.animate = animate;
+      this.options = options;
+
+      this.overlaysTrack = getTrackObject(definition.overlaysTrack);
+
+      if (!this.overlaysTrack) {
+        console.warn(
+          `Overlays track (uid: ${definition.overlaysTrack}) not found`
+        );
+        return;
+      }
+
+      this.annotationTrackIds = new Set();
+      this.annotationTracks = definition.options.annotationTracks
+        .map((uid) => {
+          const track = getTrackObject(uid);
+
+          if (!track) {
+            console.warn(`Child track (uid: ${uid}) not found`);
+          } else {
+            this.annotationTrackIds.add(track.uuid);
+          }
+
+          return track;
+        })
+        .filter((track) => track);
+
+      this.annotationDrawnHandlerBound = this.annotationDrawnHandler.bind(this);
+
+      this.drawnAnnotations = new Map();
+      this.tracksDrawingTiles = new Set();
+
+      this.pubSubs = [];
+
+      // Augment annotation tracks
+      this.annotationTracks.forEach((track) => {
+        track.subscribe('annotationDrawn', this.annotationDrawnHandlerBound);
+      });
+
+      this.pubSubs.push(
+        pubSub.subscribe(
+          'TiledPixiTrack.tilesDrawnEnd',
+          this.tilesDrawnEndHandler.bind(this)
+        )
+      );
+    }
+
+    /**
+     * Handles annotation drawn events
+     *
+     * @param  {String}  event.uid  UID of the view that triggered the event.
+     * @param  {Array}  event.viewPos  View position (i.e., [x, y, width, height])
+     *   of the drawn annotation on the screen.
+     * @param  {Array}  event.dataPos  Data position of the drawn annotation. For
+     *   example base pairs (Hi-C), or pixels (gigapixel images), or lng-lat
+     *   (geo json).
+     */
+    annotationDrawnHandler({ xStartAbs, xEndAbs, data }) {
+      this.drawnAnnotations.set(data.uid, [xStartAbs, xEndAbs]);
+    }
+
+    /**
+     * Remove this track.
+     */
+    remove() {
+      this.pubSubs.forEach(pubSub.unsubscribe);
+      this.pubSubs = undefined;
+      this.annotationTracks.forEach((track) => {
+        track.unsubscribe('annotationDrawn', this.annotationDrawnHandlerBound);
+      });
+    }
+
+    /**
+     * Callback function passed into the annotation tracks to trigger tree
+     * building of the spatial RTree.
+     *
+     * @description
+     * Simple counter that call `this.buildTree()` once the number of annotation
+     * tracks is reached.
+     */
+    tilesDrawnEndHandler({ uuid }) {
+      if (!this.annotationTrackIds.has(uuid)) return;
+
+      this.tracksDrawingTiles.add(uuid);
+
+      if (!(this.tracksDrawingTiles.size % this.annotationTracks.length)) {
+        this.updateOverlays();
+      }
+    }
+
+    updateOverlays() {
+      this.overlaysTrack.options.extent = Array.from(
+        this.drawnAnnotations.values()
+      );
+      this.overlaysTrack.draw();
+      this.tracksDrawingTiles.clear();
+    }
+  }
+
+  return new AnnotationOverlayMetaTrack(...args);
+}
+
+createAnnotationOverlayMetaTrack.config = {
+  type: 'annotation-overlay',
+  availableOptions: ['annotationTracks'],
+  defaultOptions: {
+    annotationTracks: [],
+  },
+};
+
+export default createAnnotationOverlayMetaTrack;
---
+++ src/tss-track.js
@@ -0,0 +1,115 @@
+import createPubSub from 'pub-sub-es';
+
+function createTssTrack(HGC, ...args) {
+  if (!new.target) {
+    throw new Error(
+      'Uncaught TypeError: Class constructor cannot be invoked without "new"'
+    );
+  }
+
+  class TssTrack extends HGC.tracks.BedLikeTrack {
+    constructor(context, options) {
+      super(context, options);
+
+      const { publish, subscribe, unsubscribe } = createPubSub();
+      this.publish = publish;
+      this.subscribe = subscribe;
+      this.unsubscribe = unsubscribe;
+    }
+
+    drawPoly(tile, xStartPos, xEndPos, rectY, doubleRadius = false) {
+      // prettier-ignore
+      const drawnPoly = [
+        // left top
+        xStartPos, rectY,
+        // right top
+        xEndPos, rectY,
+        // right bottom
+        xEndPos, this.dimensions[1],
+         // left bottom
+        xStartPos, this.dimensions[1]
+      ];
+
+      const anchorRadius = 1;
+      const radius = 2;
+
+      tile.rectGraphics.drawCircle(
+        xStartPos,
+        this.finalDotYPos(rectY, anchorRadius),
+        radius
+      );
+
+      return drawnPoly;
+    }
+
+    renderRows(tile, rows, maxRows, startY, endY, fill) {
+      const zoomLevel = +tile.tileId.split('.')[0];
+
+      this.initialize();
+
+      const focusRegion = this.options.focusRegion || [Infinity, Infinity];
+
+      const circleDraws = [];
+      const circleFocusDraws = [];
+
+      for (let j = 0; j < rows.length; j++) {
+        for (let i = 0; i < rows[j].length; i++) {
+          const td = rows[j][i].value;
+          const geneInfo = td.fields;
+
+          // the returned positions are chromosome-based and they need to
+          // be converted to genome-based
+          const chrOffset = +td.chrOffset;
+          const txStart = +geneInfo[1] + chrOffset;
+          const txEnd = +geneInfo[2] + chrOffset;
+
+          const xStartPos = this._xScale(txStart);
+          const xEndPos = this._xScale(txEnd);
+
+          // don't draw anything that has already been drawn
+          if (
+            !(
+              zoomLevel in this.drawnRects &&
+              td.uid in this.drawnRects[zoomLevel]
+            )
+          ) {
+            if (!this.drawnRects[zoomLevel]) this.drawnRects[zoomLevel] = {};
+
+            const circleDraw = {
+              xStartScreen: xStartPos,
+              xEndScreen: xEndPos,
+              xStartAbs: txStart,
+              xEndAbs: txEnd,
+              data: td,
+            };
+
+            if (txStart <= focusRegion[1] && txEnd >= focusRegion[0]) {
+              circleFocusDraws.push(circleDraw);
+            } else {
+              circleDraws.push(circleDraw);
+            }
+          }
+        }
+
+        circleDraws.forEach((circleDraw) => {
+          this.publish('annotationDrawn', circleDraw);
+        });
+      }
+    }
+
+    rerender() {
+      // Monkey patch because TiledPixiTrack doesn't always fire correctly
+      this.draw();
+      this.pubSub.publish('TiledPixiTrack.tilesDrawnEnd', { uuid: this.uuid });
+    }
+  }
+
+  return new TssTrack(...args);
+}
+
+createTssTrack.config = {
+  type: 'tss',
+  datatype: ['bedlike'],
+};
+
+export default createTssTrack;
