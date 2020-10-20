55e10d61411f758f5630123638609b2b55da4662
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-10-20 12:43:10 -0400

    Updates

---
+++ precache-manifest.5fce5569bc1cbc0a30027fc5d775b4c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca62f2debf61cb699c346180a17647ab",
+    "url": "/enhancer-promoter-vis/index.html"
+  },
+  {
+    "revision": "c6f2796453a1216105ff",
+    "url": "/enhancer-promoter-vis/static/css/2.99e59d71.chunk.css"
+  },
+  {
+    "revision": "7ea1879e8e204b0a392d",
+    "url": "/enhancer-promoter-vis/static/css/main.54caf944.chunk.css"
+  },
+  {
+    "revision": "c6f2796453a1216105ff",
+    "url": "/enhancer-promoter-vis/static/js/2.745c4ccc.chunk.js"
+  },
+  {
+    "revision": "7dfd170b0073c5c684c26bd4f12baa2e",
+    "url": "/enhancer-promoter-vis/static/js/2.745c4ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea1879e8e204b0a392d",
+    "url": "/enhancer-promoter-vis/static/js/main.775a9bf4.chunk.js"
+  },
+  {
+    "revision": "461d40da56e0b3288261",
+    "url": "/enhancer-promoter-vis/static/js/runtime-main.63aa7f26.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/enhancer-promoter-vis/precache-manifest.8e62f13d97c47d19bc0e00040eb88b02.js"
+  "/enhancer-promoter-vis/precache-manifest.5fce5569bc1cbc0a30027fc5d775b4c5.js"
 );
 
 self.addEventListener('message', (event) => {
