7cf0492d3f7280051be73788859521bf3e1bd5be
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-12-16 23:00:56 -0500

    Remove unused inputs

--- src/ModalProvider.js
+++ src/ModalProvider.js
@@ -3,7 +3,6 @@ import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
-import { identity } from '@flekschas/utils';
 
 const ModalContext = createContext();
 
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,5 @@
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
