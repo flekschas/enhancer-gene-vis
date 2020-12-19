49731d31655632ff018ec12eb8e8d92d9cb23134
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-12-19 17:42:18 -0500

    Improve information text

--- src/EnhancerGenesInfo.js
+++ src/EnhancerGenesInfo.js
@@ -4,7 +4,11 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { enhancerGenesShowInfoState, focusVariantState } from './state';
+import {
+  enhancerGenesCellEncodingState,
+  enhancerGenesShowInfoState,
+  focusVariantState,
+} from './state';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,16 +17,32 @@ const useStyles = makeStyles((theme) => ({
   },
   text: {
     fontSize: '0.8rem',
+    verticalAlign: 'center',
   },
   placeholder: {
     fontStyle: 'italic',
     color: theme.palette.grey['600'],
+  },
+  small: {
+    fontSize: '0.5em',
+    lineHeight: '2em',
+  },
+  medium: {
+    fontSize: '1em',
+    lineHeight: '1em',
+  },
+  large: {
+    fontSize: '1.5em',
+    lineHeight: '0.6667em',
+    marginLeft: '-0.05rem',
+    marginRight: '0.1rem',
   },
 }));
 
 const EnhancerRegionsInfo = React.memo(function EnhancerRegionsInfo() {
   const showInfo = useRecoilValue(enhancerGenesShowInfoState);
   const focusVariant = useRecoilValue(focusVariantState);
+  const cellEncoding = useRecoilValue(enhancerGenesCellEncodingState);
 
   const classes = useStyles();
 
@@ -30,12 +50,7 @@ const EnhancerRegionsInfo = React.memo(function EnhancerRegionsInfo() {
 
   return (
     <div>
-      {focusVariant ? (
-        <Typography className={classes.text}>
-          Enhancer regions overlapping <em>{focusVariant}</em> and its predicted
-          connections to upstream (left) and downstream (right) genes.
-        </Typography>
-      ) : (
+      {!focusVariant && (
         <Grid
           container
           justify="center"
@@ -46,6 +61,49 @@ const EnhancerRegionsInfo = React.memo(function EnhancerRegionsInfo() {
             Select a variant to see details
           </Typography>
         </Grid>
+      )}
+      {focusVariant && cellEncoding === 'number' && (
+        <React.Fragment>
+          <Typography className={classes.text}>
+            <span className={classes.small}>■</span>
+            <span className={classes.medium}>■</span>
+            <span className={classes.large}>■</span>
+            <strong>Absolute number</strong> of enhancer-gene connections
+            overlapping <em>{focusVariant}</em> by sample groups across
+            up/downstream genes.
+          </Typography>
+        </React.Fragment>
+      )}
+      {focusVariant && cellEncoding === 'percent' && (
+        <React.Fragment>
+          <Typography className={classes.text}>
+            <span className={classes.small}>■</span>
+            <span className={classes.medium}>■</span>
+            <span className={classes.large}>■</span>
+            <strong>Percentage</strong> of enhancer-gene connections overlapping{' '}
+            <em>{focusVariant}</em> within a sample group across up/downstream
+            genes.
+          </Typography>
+        </React.Fragment>
+      )}
+      {focusVariant && cellEncoding === 'distribution' && (
+        <React.Fragment>
+          <Typography className={classes.text}>
+            <strong>Distribution</strong> of the prediction scores of
+            enhancer-gene connections overlapping <em>{focusVariant}</em> by
+            sample group across up/downstream genes.
+          </Typography>
+        </React.Fragment>
+      )}
+      {focusVariant && cellEncoding === 'array' && (
+        <React.Fragment>
+          <Typography className={classes.text}>
+            <span>■</span>
+            <strong>Presence</strong> of enhancer-gene connections overlapping{' '}
+            <em>{focusVariant}</em> by samples across sample groups and
+            up/downstream genes.
+          </Typography>
+        </React.Fragment>
       )}
     </div>
   );
