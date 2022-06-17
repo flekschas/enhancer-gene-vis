4c516088bc848e90a13c2ccc424f0fc2247f3062
Riya Sinha <riya.v.sinha@gmail.com>
2022-06-17 16:46:07 -0700

    Add comments and clean up query string util

--- src/utils/query-string.ts
+++ src/utils/query-string.ts
@@ -7,15 +7,24 @@ export type QueryStringDecoder<T> = (
   x: string | string[] | null | undefined
 ) => T;
 
+/**
+ * Subscribes to changes to an atom, and provides a function to synchronously
+ * modify the Recoil state and window query string state
+ *
+ * @param key The key to update
+ * @param atom The atom to listen to and update
+ * @param encoder A function that encodes the atom value as a string
+ * @returns A tuple of the atom value and a function to modify it
+ */
 export function useRecoilQueryString<T>(
   key: string,
   atom: RecoilState<T>,
   encoder: QueryStringEncoder<T>
-) {
+): [T, (x: T) => void] {
   const [value, setValue] = useRecoilState(atom);
 
   const onSetValue = useCallback(
-    (newValue) => {
+    (newValue: T) => {
       setValue(newValue);
       setQueryStringValue(key, newValue, encoder);
     },
@@ -25,11 +34,19 @@ export function useRecoilQueryString<T>(
   return [value, onSetValue];
 }
 
+/**
+ * Subscribes to changes to the atom value, and updates the key value in the
+ * query string accordingly.
+ *
+ * @param key The key to update
+ * @param atom The atom to listen to changes for
+ * @param encoder A function that encodes the atom value as a string
+ */
 export function useRecoilQueryStringSyncher<T>(
   key: string,
   atom: RecoilState<T>,
   encoder: QueryStringEncoder<T>
-) {
+): void {
   const value = useRecoilValue(atom);
 
   useMemo(() => {
@@ -37,6 +54,14 @@ export function useRecoilQueryStringSyncher<T>(
   }, [key, encoder, value]);
 }
 
+/**
+ * Returns a decoded query string value for a provided key.
+ *
+ * @param key The query string key to look for
+ * @param decoder A function that parses the query string value into a semantic
+ *    type
+ * @returns The decoded value, if it exists.
+ */
 export function getQueryStringValue<T>(
   key: string,
   decoder: QueryStringDecoder<T>
@@ -45,10 +70,11 @@ export function getQueryStringValue<T>(
 }
 
 /**
+ * Sets a query string value into the browser URL.
  *
- * @param key query parameter key
- * @param value
- * @param encoder
+ * @param key The query string key to set
+ * @param value The value to be encoded into the URL for the key
+ * @param encoder A function that converts the value to a string
  */
 export function setQueryStringValue<T>(
   key: string,
