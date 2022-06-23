922b226620eb1e21637f44d4c377e28cc125e61f
Riya Sinha <33243383+riyavsinha@users.noreply.github.com>
2022-06-23 11:37:34 -0700

    Type ChromosomeInfo correctly (#22)

--- src/types/higlass.d.ts
+++ src/types/higlass.d.ts
@@ -1,8 +1,15 @@
 declare module 'higlass' {
   function ChromosomeInfo(source: string): Promise<ChromosomeInfoResult>;
   class ChromosomeInfoResult {
-    // static constructor(source: string): Promise<ChromosomeInfo>;
-    chromosome: string;
-    totalLength?: number;
+    /** Mapping of chromosome to ChromosomePosition object */
+    chrPositions: {[key: string]: ChromosomePosition};
+    chromLengths: {[key: string]: number};
+    cumPositions: ChromosomePosition[];
+    totalLength: number;
+  }
+  type ChromosomePosition = {
+    id: number;
+    chr: string;
+    pos: number;
   }
 }
