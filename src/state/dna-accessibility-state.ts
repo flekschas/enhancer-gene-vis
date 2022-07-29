9a355f7a9fd812a14e448d29bfb58e4fe87586c6
Riya Sinha <33243383+riyavsinha@users.noreply.github.com>
2022-07-29 12:29:17 -0700

    Type part of DNA Accessibility state and settings component (#31)
    * Move some dna accessibility state types to typed file
    
    * Update imports and type dna accessibility state
    
    * lint

---
+++ src/state/dna-accessibility-state.ts
@@ -0,0 +1,67 @@
+import { identity } from 'lodash-es';
+import { atom, RecoilState } from 'recoil';
+import { booleanQueryStringDecoder } from '../utils';
+import {
+  booleanQueryStringEncoder,
+  useRecoilQueryString,
+  useRecoilQueryStringSyncher,
+} from '../utils/query-string';
+import { getDefault } from './utils';
+
+export enum DnaAccessibilityLabelStyle {
+  INDICATOR = 'indicator',
+  TEXT = 'text',
+  HIDDEN = 'hidden',
+}
+
+export const dnaAccessLabelStyleState: RecoilState<DnaAccessibilityLabelStyle> = atom(
+  {
+    key: 'dnaAccessLabelStyle',
+    default: getDefault<DnaAccessibilityLabelStyle>(
+      'dal',
+      DnaAccessibilityLabelStyle.INDICATOR,
+      identity
+    ),
+  }
+);
+
+export const dnaAccessRowNormState: RecoilState<boolean> = atom({
+  key: 'dnaAccessRowNorm',
+  default: getDefault('darn', true, booleanQueryStringDecoder),
+});
+
+export const dnaAccessLabelShowInfoState: RecoilState<boolean> = atom({
+  key: 'dnaAccessLabelShowInfos',
+  default: getDefault('dai', true, booleanQueryStringDecoder),
+});
+
+export const useDnaAccessLabelStyle = () =>
+  useRecoilQueryString('dal', dnaAccessLabelStyleState, identity);
+export const useDnaAccessLabelStyleSyncher = () =>
+  useRecoilQueryStringSyncher('dal', dnaAccessLabelStyleState, identity);
+
+export const useDnaAccessRowNorm = () =>
+  useRecoilQueryString(
+    'darn',
+    dnaAccessRowNormState,
+    booleanQueryStringEncoder
+  );
+export const useDnaAccessRowNormSyncher = () =>
+  useRecoilQueryStringSyncher(
+    'darn',
+    dnaAccessRowNormState,
+    booleanQueryStringEncoder
+  );
+
+export const useDnaAccessShowInfos = () =>
+  useRecoilQueryString(
+    'dai',
+    dnaAccessLabelShowInfoState,
+    booleanQueryStringEncoder
+  );
+export const useDnaAccessShowInfosSyncher = () =>
+  useRecoilQueryStringSyncher(
+    'dai',
+    dnaAccessLabelShowInfoState,
+    booleanQueryStringEncoder
+  );
