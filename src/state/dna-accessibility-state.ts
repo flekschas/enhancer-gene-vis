4895ebadc34255093b6fbda6ccfae17d26340f13
Riya Sinha <riya.v.sinha@gmail.com>
2022-08-16 23:44:17 -0700

    Add DNA accessibility track info to url

--- src/state/dna-accessibility-state.ts
+++ src/state/dna-accessibility-state.ts
@@ -1,5 +1,6 @@
 import { identity } from 'lodash-es';
 import { atom, RecoilState } from 'recoil';
+import { DEFAULT_DNA_ACCESSIBILITY_TRACK_UID } from '../components/dna-accessibility/constants-dna-accessibility';
 import { booleanQueryStringDecoder } from '../utils';
 import {
   booleanQueryStringEncoder,
@@ -7,7 +8,75 @@ import {
   useRecoilQueryStringSyncher,
 } from '../utils/query-string';
 import { RidgePlotTrackLabelStyle } from '../view-config-types';
-import { getDefault } from './utils';
+import { DEFAULT_ENHANCER_REGION_SERVER_ABBR } from './enhancer-region-state';
+import {
+  getDefault,
+  SERVER_URL_TO_TRACK_SOURCE_ABBR,
+  TrackSourceAbbr,
+  TRACK_SOURCE_ABBR_TO_SERVER_URL,
+} from './utils';
+
+export type DnaAccessibilityTrackInfo = {
+  server: string;
+  tilesetUid: string;
+  label: string;
+};
+
+export const DEFAULT_DNA_ACCESSIBILITY_TRACK_INFO: DnaAccessibilityTrackInfo = {
+  server: 'https://resgen.io/api/v1',
+  tilesetUid: DEFAULT_DNA_ACCESSIBILITY_TRACK_UID,
+  label: 'DNA Accessibility',
+};
+
+export function dnaAccessibilityTrackInfoEncoder(
+  track: DnaAccessibilityTrackInfo
+): string {
+  const serverAbbr = SERVER_URL_TO_TRACK_SOURCE_ABBR[track.server];
+
+  if (!track.tilesetUid || !serverAbbr) {
+    throw new Error(
+      `Invalid DNA accessibility track encoder argument, track must have tilesetUid and valid server: ${track}`
+    );
+  }
+
+  return [track.tilesetUid, serverAbbr, track.label].join(':');
+}
+
+/**
+ * Decodes a string representing an enhancer region track back into an Enhancer Track object
+ *
+ * @param v The variant track string to decode
+ * @returns An array containing a single variant track
+ */
+function dnaAccessibilityTrackInfoDecoder(
+  v?: string
+): DnaAccessibilityTrackInfo {
+  if (!v) throw new Error(`No string provided to variant track decoder`);
+
+  // Example: tilesetId:rg
+  const [tilesetUid, serverAbbr = DEFAULT_ENHANCER_REGION_SERVER_ABBR, label] =
+    v.split(':');
+
+  const server = TRACK_SOURCE_ABBR_TO_SERVER_URL[serverAbbr as TrackSourceAbbr];
+
+  if (tilesetUid === undefined) return tilesetUid;
+
+  return {
+    server,
+    tilesetUid,
+    label,
+  } as DnaAccessibilityTrackInfo;
+}
+
+export const dnaAccessibilityTrackState: RecoilState<DnaAccessibilityTrackInfo> =
+  atom({
+    key: 'dats',
+    default: getDefault(
+      'dats',
+      DEFAULT_DNA_ACCESSIBILITY_TRACK_INFO,
+      dnaAccessibilityTrackInfoDecoder
+    ),
+  });
 
 export const dnaAccessLabelStyleState: RecoilState<RidgePlotTrackLabelStyle> =
   atom({
@@ -28,6 +97,19 @@ export const dnaAccessLabelShowInfoState: RecoilState<boolean> = atom({
   key: 'dnaAccessLabelShowInfos',
   default: getDefault('dai', true, booleanQueryStringDecoder),
 });
+
+export const useDnaAccessibilityTrack = () =>
+  useRecoilQueryString(
+    'dats',
+    dnaAccessibilityTrackState,
+    dnaAccessibilityTrackInfoEncoder
+  );
+export const useDnaAccessibilityTrackSyncher = () =>
+  useRecoilQueryStringSyncher(
+    'dats',
+    dnaAccessibilityTrackState,
+    dnaAccessibilityTrackInfoEncoder
+  );
 
 export const useDnaAccessLabelStyle = () =>
   useRecoilQueryString('dal', dnaAccessLabelStyleState, identity);
